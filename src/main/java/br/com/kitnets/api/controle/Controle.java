60b7a47bdad908b6fa7915af4aa35eb9f18530e6
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-04-25 11:16:04 -0300

    teste3

--- src/main/java/br/com/kitnets/api/controle/Controle.java
+++ src/main/java/br/com/kitnets/api/controle/Controle.java
@@ -3,6 +3,8 @@ package br.com.kitnets.api.controle;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -87,7 +89,7 @@ public class Controle {
     }
 
     @GetMapping("")
-    public String mensagem(){
+    public String mensagem(){   // teste 3
         return"heeeello Word";
     }
     
@@ -107,8 +109,8 @@ public class Controle {
     }
 
     @GetMapping("/status")
-    public String status(){
-        return "Configurando status";
+    public ResponseEntity<?> status(){
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     
